8ca91e604042730104165a497d5ba13e3b8f8cf4
dvthnight <davide.lanotte@protonmail.com>
2022-03-07 17:42:38 +0100

    apportato alcune modifiche

--- src/components/JumboHero.vue
+++ src/components/JumboHero.vue
@@ -1,15 +1,33 @@
 <template>
-  <div class="titolo">
-    <img src="https://picsum.photos/200/300" alt="">
+  <div class="jumbo">
+    <h2>Questo è il jumbo</h2>
   </div>
 </template>
 
 <script>
 export default {
-  
+  name: "JumboHero"
 }
 </script>
 
 
 <style scoped>
+
+
+
+.jumbo{
+  background-image: url("https://picsum.photos/2000/400");
+  height: 500px;
+  background-size: cover;
+  position: relative;
+}
+
+h2{
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  color: white;
+  text-transform: uppercase;
+}
 </style>
--- src/components/MyTitle.vue
+++ src/components/MyTitle.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="titolo">
-    <h1>Ciao</h1>
+    <h1>La mia prima app con Vue CLI!</h1>
   </div>
 </template>
 
 <script>
 export default {
-  
+  name: "MyTitle"
 }
 </script>
 
